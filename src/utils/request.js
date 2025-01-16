d2d542808808b1cbbd2f1ca768ccb7581b9dadf5
Younglina <younglina0409@outlook.com>
2025-01-16 13:45:23 +0800

    feat: api

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 // import useUserStore from '@/store/user.js' //引入仓库，使用的是pinia
 import axios from "axios";
-const baseUrl = "https://gw.xmbus.com:31232/api-travel";
-// const baseUrl = "https://www.xmlenzrd.com:31232/api-tanji";
+// const baseUrl = "https://gw.xmbus.com:31232/api-travel";
+const baseUrl = "https://www.xmlenzrd.com:31232/api-tanji";
 const request = (options) => {
   return new Promise((resolve, reject) => {
     // 设置请求头
@@ -10,8 +10,8 @@ const request = (options) => {
       // appId: "tanji_travel",
       // timestamp,
       // sign,
-      "X-maas-Tenantid": "zh00002",
-      // "X-maas-Tenantid": "1",
+      // "X-maas-Tenantid": "zh00002",
+      "X-maas-Tenantid": "1",
       ...options.headers, // 可以传入额外的请求头参数
     };
     // 在发送请求之前执行拦截操作
